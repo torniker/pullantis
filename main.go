cb7b86313607e47d8333b58b8147d4ceb94b6e80
Tornike Razmadze <torniker@gmail.com>
2021-01-16 14:39:59 +0400

    testing

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ func HookHandler(prChan chan<- PullRequest) http.HandlerFunc {
 		// log.Printf("received event: %v\n", event)
 		switch e := event.(type) {
 		case *github.PullRequestEvent:
-			repoName := strings.Split(*e.GetRepo().Name, "/")
+			repoName := strings.Split(*e.GetRepo().FullName, "/")
 			prChan <- PullRequest{
 				Owner:  repoName[0],
 				Repo:   repoName[1],
