daf38dacf77c2af22a0e311255b24facad2f9472
Tornike Razmadze <torniker@gmail.com>
2021-01-16 13:45:12 +0400

    testing

--- main.go
+++ main.go
@@ -8,40 +8,22 @@ import (
 )
 
 type PullRequest struct {
-	SHA string
+	SHA  string
+	Repo github.Repository
 }
 
 func main() {
-
 	prChan := make(chan PullRequest)
 	go listener(prChan)
 	http.HandleFunc("/", HookHandler(prChan))
 	log.Fatal(http.ListenAndServe(":9999", nil))
-
-	// ctx := context.Background()
-	// ts := oauth2.StaticTokenSource(
-	// 	&oauth2.Token{AccessToken: "d228fb13f42f8bab2d098436038f86e1f75f8552"},
-	// )
-	// tc := oauth2.NewClient(ctx, ts)
-
-	// client := github.NewClient(tc)
-
-	// // list all repositories for the authenticated user
-
-	// repos, _, err := client.Repositories.List(ctx, "", nil)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// for repos
-	// fmt.Println(repos)
 }
 
 func listener(prChan chan PullRequest) {
 	for {
 		select {
 		case p := <-prChan:
-			log.Printf("listener got event %v\n", p)
+			log.Printf("listener got event, with repo URL: %#v\n", p.Repo)
 		}
 	}
 }
@@ -55,18 +37,17 @@ func HookHandler(prChan chan<- PullRequest) http.HandlerFunc {
 			return
 		}
 		defer r.Body.Close()
-
 		event, err := github.ParseWebHook(github.WebHookType(r), payload)
 		if err != nil {
 			log.Printf("could not parse webhook: err=%s\n", err)
 			return
 		}
-
 		// log.Printf("received event: %v\n", event)
 		switch e := event.(type) {
 		case *github.PullRequestEvent:
 			prChan <- PullRequest{
-				SHA: *e.PullRequest.Head.SHA,
+				SHA:  *e.PullRequest.Head.SHA,
+				Repo: *e.GetRepo(),
 			}
 		case *github.PullRequestReviewCommentEvent:
 			log.Printf("received PullRequestReviewCommentEvent: %v\n", e)
