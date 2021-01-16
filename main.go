278ba4f820ac3721eb30de0ac2089d7a7d230d39
Tornike Razmadze <torniker@gmail.com>
2021-01-16 14:00:35 +0400

    testing

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/google/go-github/github"
 )
@@ -24,8 +29,30 @@ func listener(prChan chan PullRequest) {
 	for {
 		select {
 		case p := <-prChan:
-			// https://github.com/username/projectname/archive/commitshakey.zip
-			downloadURL := fmt.Sprintf("%s/%s", p.URL, p.SHA)
+			downloadURL := fmt.Sprintf("%s/archive/%s.zip", p.URL, p.SHA)
+			resp, err := http.Get(downloadURL)
+			if err != nil {
+				log.Printf("err: %s", err)
+			}
+
+			defer resp.Body.Close()
+			if resp.StatusCode != 200 {
+				continue
+			}
+			zipFile := fmt.Sprintf("./tmp/%s.zip", p.SHA)
+			out, err := os.Create(zipFile)
+			if err != nil {
+				log.Printf("error creating file: %s", err)
+			}
+			defer out.Close()
+			_, err = io.Copy(out, resp.Body)
+			if err != nil {
+				log.Printf("error copying data into file: %s", err)
+			}
+			_, err = Unzip(zipFile, fmt.Sprintf("./tmp/%s", p.SHA))
+			if err != nil {
+				log.Printf("error unziping: %s", err)
+			}
 			log.Printf("listener got event: %#v, url: %s\n", p.SHA, downloadURL)
 		}
 	}
@@ -59,4 +86,48 @@ func HookHandler(prChan chan<- PullRequest) http.HandlerFunc {
 			return
 		}
 	}
+}
+
+// Unzip does what is says
+func Unzip(src string, dest string) ([]string, error) {
+	var filenames []string
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+	for _, f := range r.File {
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: illegal file path", fpath)
+		}
+		filenames = append(filenames, fpath)
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+		// Make File
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return filenames, err
+		}
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+		_, err = io.Copy(outFile, rc)
+		// Close the file without defer to close before next iteration of loop
+		outFile.Close()
+		rc.Close()
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
 }
